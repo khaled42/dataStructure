90650bbe3970c79c4d25eb77e598fd1191f56a52
khaled42 <khaled.kaddour.alg@gmail.com>
2019-12-29 11:03:30 +0100

    implement the stack structuer

--- data_structures.py
+++ data_structures.py
@@ -1,1 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
 
+class stack:
+    def __init__(self):
+        self.head = None
+
+    def put(self, value):
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
+    def pop(self):
+        x = self.head.data
+        self.head = self.head.next
+        return x
+
+    def print_stack(self):
+        temp = self.head
+        while temp != None:
+            print(temp.data, ' -> ', end="")
+            temp = temp.next
+        print()
+
+    def size(self):
+        cpt = 0
+        temp = self.head
+        while temp != None:
+            cpt += 1
+            temp = temp.next
+        return cpt
