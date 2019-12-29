1bd90211c7533663ca891fe0f0b243f7c8d2da4f
khaled42 <khaled.kaddour.alg@gmail.com>
2019-12-29 11:41:06 +0100

    test the tow structures

--- data_structures.py
+++ data_structures.py
@@ -72,3 +72,27 @@ class Queue:
             cpt += 1
             temp = temp.next
         return cpt
+
+# ================ test ===============
+if __name__ == "__main__":
+    arry = [1, 2, 3, 4]
+
+    stack = Stack()
+    queue = Queue()
+
+    for e in arry:
+        stack.put(e)
+        queue.put(e)
+    print('stack : ', end=" ")
+    stack.print_stack()
+    print('queue : ', end=" ")
+    queue.print_queue()
+
+    print('===============================')
+    print('pop methode (stack) : ', stack.pop())
+    print('pop methode (queue) : ', queue.pop())
+    print('===============================')
+    print('stack : ', end=" ")
+    stack.print_stack()
+    print('queue : ', end=" ")
+    queue.print_queue()
